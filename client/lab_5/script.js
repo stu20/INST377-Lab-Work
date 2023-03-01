3205cee3e8a2c18b1573c33986d98859185599ee
stu20 <102126975+stu20@users.noreply.github.com>
2023-03-01 12:46:03 -0500

    Seeing if fetch in .js works

--- client/lab_5/script.js
+++ client/lab_5/script.js
@@ -3,6 +3,16 @@
   by adding `<script src="script.js">` just before your closing `</body>` tag
 */
 
+// Set currentList to an empty array
+let currentList = [];
+
+function filterList(keyword, list) {
+  const RestaurantList = currentList.filter((restaurant) => {
+    return restaurant.name.toLowerCase().includes(keyword.toLowerCase())
+  });
+  return RestaurantList
+}
+
 async function mainEvent() { // the async keyword means we can make API requests
   const form = document.querySelector('.main_form'); // This class name needs to be set on your form before you can listen for an event on it
   form.addEventListener('submit', async (submitEvent) => { // async has to be declared on every function that needs to "await" something
@@ -52,6 +62,8 @@ async function mainEvent() { // the async keyword means we can make API requests
 
     // This changes the response from the GET into data we can use - an "object"
     const arrayFromJson = await results.json();
+    currentList = arrayFromJson;
+    const RestaurantList = filterList(formProps)
     console.table(arrayFromJson.data); // this is called "dot notation"
     // arrayFromJson.data - we're accessing a key called 'data' on the returned object
     // it initially contains all 1,000 records from your request
