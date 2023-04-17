e4fc4d9a095759e0db3c6331be2a0ecb57678935
stu20 <102126975+stu20@users.noreply.github.com>
2023-04-16 23:14:01 -0400

    promises are meant to be broken

--- client/lab_8/script.js
+++ client/lab_8/script.js
@@ -1,4 +1,4 @@
-function initMap() {
+  function initMap() {
     var map = L.map('map').setView([38.9866, -76.94], 15);
   
     // add a tile layer to the map
@@ -7,34 +7,23 @@ function initMap() {
     }).addTo(map);    
 
     return map;
-}
+  }
 
-function markerPlace(coordsArray, map) {
-  // remove existing markers from the map
-  map.eachLayer(function(layer) {
-    if (layer instanceof L.Marker) {
-      map.removeLayer(layer);
-    }
-  });
-  
-  // add a marker for each set of coordinates in the array
-  coordsArray.forEach(function(coords) {
-    var latlng = L.latLng(coords[1], coords[0]);
-    var marker = L.marker(latlng).addTo(map);
-  });
-  
-  // pan the map to the first set of coordinates in the array
-  var firstCoords = coordsArray[0];
-  var firstLatlng = L.latLng(firstCoords[1], firstCoords[0]);
-  map.panTo(firstLatlng);
-}
-
-function getRandomIntInclusive(min, max) {
+  function markerPlace(map, storedList) {
+    storedList.forEach(restaurant => {
+      if (restaurant.geocoded_column_1 && restaurant.geocoded_column_1.coordinates) {
+        const coords = restaurant.geocoded_column_1.coordinates;
+        const marker = L.marker([coords[1], coords[0]]).addTo(map);
+      }
+    });
+  }
+   
+  function getRandomIntInclusive(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
   }
-   
+
   function injectHTML(list) {
     console.log('fired injectHTML');
     const target = document.querySelector('#restaurant_list');
@@ -74,21 +63,10 @@ function getRandomIntInclusive(min, max) {
     let currentList = []; // this is "scoped" to the main event function
 
     var map = initMap();
-    var coordsArray = [newList];
-    markerPlace(coordsArray, map);
-
-    // wait for the map to load
-    await new Promise(resolve => map.on('load', resolve));
-
-    // do something with the map, e.g. add markers, layers, etc.
-    L.marker([38.8985, -77.0370]).addTo(map)
-      .bindPopup('Washington Monument')
-      .openPopup();    
     
     loadDataButton.addEventListener('click', async (submitEvent) => { // async has to be declared on every function that needs to "await" something
       console.log('loading data'); 
       loadAnimation.style.display = 'inline-block';
-      //submitEvent.preventDefault();
   
       const results = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
   
@@ -111,10 +89,18 @@ function getRandomIntInclusive(min, max) {
 
       console.log(formProps);
       const filteredList = filterList(storedList, formProps.resto);
+      
+      markerPlace(map, filteredList);
 
       console.log(filteredList);
       injectHTML(filteredList);
-    });
+      
+      // Check if the first element of filteredList has latitude and longitude properties
+      // Unable to get the code to pan at the moment. Will debug. Maybe setView is overriding panTo method?
+      if (filteredList[0].latitude && filteredList[0].longitude) {
+        map.panTo({ lat: filteredList[0].latitude, lng: filteredList[0].longitude });
+      };
+    }); 
   
     generateListButton.addEventListener('click', (event) => {
       console.log('generate new list')
